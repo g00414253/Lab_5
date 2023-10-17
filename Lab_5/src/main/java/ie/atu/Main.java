5fd6158a15504734d08ec6ed1765189d9216e12e
G00414253@atu.ie <g00414253@atu.ie>
2023-10-17 14:13:00 +0100

    Feature 1 made with class for person's

--- Lab_5/src/main/java/ie/atu/Main.java
+++ Lab_5/src/main/java/ie/atu/Main.java
@@ -1,12 +1,4 @@
 package ie.atu;
 
 public class Main {
-    public static void main(String[] args){
-        Person johnDoe = new Person("John","Doe",20);
-        Person Blank = new Person();
-
-
-        johnDoe.displayInfo();
-        Blank.displayInfo();
-    }
 }
